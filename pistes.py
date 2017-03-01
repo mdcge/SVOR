d953aa0494d2e967e9c4055ebfecac455f7a60ff
Max Generowicz <mdcg-git@my-post-office.net>
2017-03-01 20:07:52 +0100

    Add objective function and test
    Implemented as the sum of the cumulative sum of the distances between
    adjacent points in the path.

--- pistes.py
+++ pistes.py
@@ -1,3 +1,6 @@
+from math import sqrt
+import numpy as np
+
 coordinates = []
 
 with open('coordinates.txt') as data:
@@ -5,3 +8,15 @@ with open('coordinates.txt') as data:
         _, x, y = map(int, line.split())
         coordinates.append((x,y))
 
+def total_wait_time(order, pos=coordinates):
+    distances = [ distance(pos[a], pos[b])
+                  for a,b in zip(order[1:], order[:-1]) ]
+    wait_times = np.cumsum(distances)
+    return round(wait_times.sum())
+
+def distance(a,b):
+    xa, ya = a
+    xb, yb = b
+    dx = xa - xb
+    dy = ya - yb
+    return sqrt(dx*dx + dy*dy)
--- pistes_test.py
+++ pistes_test.py
@@ -1,4 +1,7 @@
-from pistes import coordinates
+from pytest import mark
+parametrize = mark.parametrize
+
+from pistes import (coordinates, distance, total_wait_time)
 
 
 def test_30_coordinates():
@@ -15,3 +18,43 @@ def test_start_at_origin():
     assert coordinates[0] == (0,0)
     for p in coordinates[1:]:
         assert p != (0,0)
+
+
+
+@parametrize('a b d'.split(),
+   ((( 0, 0), ( 0, 1), 1),
+    (( 0, 0), ( 3, 4), 5),
+    (( 1, 1), ( 4, 5), 5),
+    ((-1,-1), (-4,-5), 5),
+))
+def test_distance(a, b, d):
+    assert distance(a,b) == d
+
+
+_2_3_swap = list(range(30))
+_2_3_swap[1], _2_3_swap[2] = _2_3_swap[2], _2_3_swap[1]
+
+
+_swap_end = list(range(30))
+_swap_end[-1], _swap_end[-2] = _swap_end[-2], _swap_end[-1]
+
+
+@parametrize('order cost positions'.split(),
+    (
+  # One step of length 1, cost: 1
+  ((0,1),     1, ((0,0), (0,1))),
+  # Two steps of length 1 each, cost: 1+2 = 3
+  ((0,1,2),   3, ((0,0), (0,1), (0,2))),
+  # Three steps of lenth 1 each, cost: 1+2+3 = 6
+  ((0,1,2,3), 6, ((0,0), (0,1), (0,2), (0,3))),
+  # As above, but with a bend
+  ((0,1,2,3), 6, ((0,0), (0,1), (1,1), (2,1))),
+  # Taken from spreadsheet: in order
+  (range(30), 212125, coordinates),
+  # Taken from spreadsheet: 2nd and 3rd swapped
+  (_2_3_swap, 211470, coordinates),
+  # Taken from spreadsheet: last two swapped
+  (_swap_end, 212572, coordinates),
+    ))
+def test_total_wait_time(order, cost, positions):
+    assert total_wait_time(order, positions) == cost
